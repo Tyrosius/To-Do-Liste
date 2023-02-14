749a6ab8d6e185110f3b4080205630fdf5f3ac03
Gerald <53179363+Gercai@users.noreply.github.com>
2023-02-14 14:57:30 +0000

    pseudocode

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const forms = document.querySelectorAll("form");
+
+forms.forEach((form) =>
+  form.addEventListener("submit", (e) => e.preventDefault())
+);
+
 function addList() { alert('not implemented') };
 function editList() { alert('not implemented') };
 function deleteList() { alert('not implemented') };
@@ -7,3 +13,28 @@ function inProgress() { alert('not implemented') };
 function done() { alert('not implemented') };
 function editToDo() { alert('not implemented') };
 function deleteToDo() { alert('not implemented') };
+
+
+//Aufgabe 1
+
+// id ab 4 incrementieren alle einmalig.
+// Auf Input value zugreifen. 
+// Pseudocode: 
+// AddButton.EventListener (Function () => ul.append(Input Value))
+
+//Aufgabe 2
+
+// edit: über Parent Element auf h3 zugreifen und editieren.
+// image ändern.
+
+//Aufgabe 3
+
+// Done -> image ändern und style von der H3 ändern
+// Progress -> image ändern style von H3?
+
+//Aufgabe 4.
+
+// löschen Pseudocode (e) => e.target.parentelement.remove
+
+
+
