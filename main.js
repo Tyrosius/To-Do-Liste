21e432428d8870a243a66cb55468704b5edfd9b6
Tyrosius <florian.schoof@gmx.de>
2023-02-14 18:08:12 +0100

    edit-funciton implemented

--- main.js
+++ main.js
@@ -11,28 +11,36 @@ function deleteList() { alert('not implemented') };
 function addToDo() { alert('not implemented') };
 function inProgress() { alert('not implemented') };
 function done() { alert('not implemented') };
-function editToDo() { alert('not implemented') };
+function editToDo(element) {
+  const currentId = element.currentTarget.parentNode.id;
+  const editButton = document.querySelector(`#${currentId}>.edit-button`);
+  const newName = prompt("Neue Bezeichnung eingeben: ", editButton.parentNode.childNodes[1].innerHTML);
+  if (newName !== "" && newName !== null) {
+    editButton.parentNode.childNodes[1].innerHTML = newName;
+  }
+};
 function deleteToDo() { alert('not implemented') };
 
 
-//Aufgabe 1
+
+//Aufgabe 1 Nico
 
 // id ab 4 incrementieren alle einmalig.
-// Auf Input value zugreifen. 
-// Pseudocode: 
+// Auf Input value zugreifen.
+// Pseudocode:
 // AddButton.EventListener (Function () => ul.append(Input Value))
 
-//Aufgabe 2
+//Aufgabe 2 Florian
 
 // edit: über Parent Element auf h3 zugreifen und editieren.
 // image ändern.
 
-//Aufgabe 3
+//Aufgabe 3 Gerald
 
 // Done -> image ändern und style von der H3 ändern
 // Progress -> image ändern style von H3?
 
-//Aufgabe 4.
+//Aufgabe 4. Anna
 
 // löschen Pseudocode (e) => e.target.parentelement.remove
 
